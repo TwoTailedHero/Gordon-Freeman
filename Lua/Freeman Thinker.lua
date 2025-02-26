688cb95fe6a8e9220ac041e62ef7ac4bd12b8388
TwoTailedHero <a.ydenblack200999@gmail.com>
2025-02-26 06:54:53 -0500

    port weapon selecting to freeman thinker

--- Lua/Freeman Thinker.lua
+++ Lua/Freeman Thinker.lua
@@ -36,3 +36,26 @@ addHook("PlayerThink", function(player)
 		player.realmo.state = S_PLAY_STND
 	end
 end)
+
+addHook("PlayerThink", function(player)
+	if not player.mo return end
+	if player.mo.skin != skin return end
+	if (player.cmd.buttons & BT_WEAPONMASK)
+		if not player.kombipressingwpnkeys
+			player.kombiaccessinghl1menu = true
+			player.kombiprevhl1category = player.kombihl1category
+			player.kombihl1category = (player.cmd.buttons & BT_WEAPONMASK)
+
+			if player.kombiprevhl1category ~= player.kombihl1category
+				player.selectionlist = HL_GetWeapons(HL_WpnStats, player.kombihl1category, player)
+				player.kombihl1wpn = 1
+			else
+				player.kombihl1wpn = player.kombihl1wpn == player.selectionlist["weaponcount"] and 1 or player.kombihl1wpn + 1
+			end
+
+			player.kombipressingwpnkeys = true
+		end
+	elseif player.kombipressingwpnkeys
+		player.kombipressingwpnkeys = false
+	end
+end)
